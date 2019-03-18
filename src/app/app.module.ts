936e6e5d54bc7b80f608c1051cdba6706a98ea48
Clark Brading <scbrading@gmail.com>
2019-03-18 08:19:11 -0400

    Beginning to configure router state.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule }                from '@angular/core';
+import { BrowserModule }           from '@angular/platform-browser';
+import { Routes, RouterModule }    from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { StoreModule, MetaReducer } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
+import { EffectsModule }            from '@ngrx/effects';
+
+import { reducers } from './store';
 
 // not used in production
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { storeFreeze } from 'ngrx-store-freeze';
+import { storeFreeze }         from 'ngrx-store-freeze';
 
 // this would be done dynamically with webpack for builds
 const environment = {
   development: true,
-  production: false,
+  production : false
 };
 
 export const metaReducers: MetaReducer<any>[] = !environment.production
@@ -25,23 +27,23 @@ import { AppComponent } from './containers/app/app.component';
 
 // routes
 export const ROUTES: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'products' },
+  {path: '', pathMatch: 'full', redirectTo: 'products'},
   {
-    path: 'products',
-    loadChildren: '../products/products.module#ProductsModule',
-  },
+    path        : 'products',
+    loadChildren: '../products/products.module#ProductsModule'
+  }
 ];
 
 @NgModule({
-  imports: [
+  imports     : [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(ROUTES),
-    StoreModule.forRoot({}, { metaReducers }),
+    StoreModule.forRoot(reducers, {metaReducers}),
     EffectsModule.forRoot([]),
-    environment.development ? StoreDevtoolsModule.instrument() : [],
+    environment.development ? StoreDevtoolsModule.instrument() : []
   ],
   declarations: [AppComponent],
-  bootstrap: [AppComponent],
+  bootstrap   : [AppComponent]
 })
 export class AppModule {}
